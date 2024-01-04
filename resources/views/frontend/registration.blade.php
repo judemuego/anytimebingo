1a6c6245e2bb9ee0b3f81cf2c8b7f85faea33560
judemuego <47876710+judemuego@users.noreply.github.com>
2024-01-04 23:51:14 +0800

    registration form

--- resources/views/frontend/registration.blade.php
+++ resources/views/frontend/registration.blade.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>RegistrationForm_v2 by Colorlib</title>
+		<title>Anytime Bingo Registration</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 		<!-- MATERIAL DESIGN ICONIC FONT -->
